890c6de16d7a0fa8ac791998efd929adc91b1e76
Dan Frost <dan@danfrost.co.uk>
2009-01-09 20:02:36 +0000

    Remove junk from help

--- phaker/core/Help.php
+++ phaker/core/Help.php
@@ -19,7 +19,7 @@ class Phake_Script_Help extends Phake_Script {
 	/**
 	 * Default help message
 	 */
-	function index($command, $req1, $verbose=true, $blar=null) {
+	function index() {
 		phake('phaker welcome-short');
 		phake('help', 'commands', @$this->args[0]);
 	}
