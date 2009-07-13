aff6044909a7835987b9a735f92ed67f446e68f7
Dan Frost <dan@danfrost.co.uk>
2009-07-13 18:45:02 +0100

    Tidy up bootstrap

--- lib/Phake/bootstrap.php
+++ lib/Phake/bootstrap.php
@@ -13,94 +13,23 @@ ini_set('include_path',ini_get('include_path').':'.dirname(dirname(__FILE__)).'/
 
 require_once 'Zend/Loader.php';
 
-// d F
-
-class Phake_AutoLoader_Exception extends Exception {}
-
-class Phake_AutoLoader_Exception_ClassUnknown extends Phake_AutoLoader_Exception {}
-
-class Phake_AutoLoader_Exception_LoaderConflict extends Phake_AutoLoader_Exception {}
-
 require_once PHAKE_DIR_SRC.'Autoloader.php';
 
 Zend_Loader::registerAutoload('Phake_AutoLoader');
 
-// *** I think this is probably all overkill...!!
+//class Phake_AutoLoader_Exception extends Exception {}
+//class Phake_AutoLoader_Exception_ClassUnknown extends Phake_AutoLoader_Exception {}
+//class Phake_AutoLoader_Exception_LoaderConflict extends Phake_AutoLoader_Exception {}
 
-abstract class Phake_AutoLoader_Loader {
-    
-    final protected function convertClassToFilename($class, $base_directory) {
-        $file = str_replace('_', '/', $class);
-        $file = $base_directory.$file.'.php';
-        return $file;
-    }
-    
-    final public function __toString() {
-        return get_class($this);
-    }
-    
-    abstract function knowsClass($class);
-    
-    abstract function getClassFile($class);
-    
-    final function loadClass($class) {
-        
-        if(!$this->knowsClass($class)) {
-            throw new Phake_AutoLoader_Exception_ClassUnknown("When trying to load class from loader");
-        }
-        
-        if(!$file=$this->getClassFile($class)) {
-            throw new Phake_AutoLoader_Exception_ClassUnknown("When trying to load class from loader");
-        }
-        
-        require_once $file;
-    }    
-}
+require_once PHAKE_DIR_SRC . '/AutoLoader/Exception.php';
+require_once PHAKE_DIR_SRC . '/AutoLoader/Exception/ClassUnknown.php';
 
-class Phake_AutoLoader_Loader_Phake extends Phake_AutoLoader_Loader {
-    function knowsClass($class) {
-        if(substr($class,0,6)=='Phake_') {
-            $file = $this->convertClassToFilename($class, dirname(PHAKE_DIR_SRC).'/');
-            return file_exists($file);
-        }
-        
-        return false;
-    }
-    
-    function getClassFile($class) {
-        if($this->knowsClass($class)) {
-            return $this->convertClassToFilename($class, dirname(PHAKE_DIR_SRC).'/');
-        }
-    }
-}
+require_once PHAKE_DIR_SRC . '/AutoLoader/Loader.php';
 
+require_once PHAKE_DIR_SRC . '/AutoLoader/Loader/Phake.php';
 Phake_AutoLoader::addAutoloader(new Phake_AutoLoader_Loader_Phake());
 
-class Phake_AutoLoader_Loader_Scripts extends Phake_AutoLoader_Loader {
-    private static $class_cache = array();
-    
-    function knowsClass($class) {
-        
-        if(substr($class,0,13)=='Phake_Script_') {
-            $file = str_replace('Phake_Script_', '', $class).'.php';
-            try {
-                $file = Phake_Finder::findFile($file);
-                self::$class_cache[$class] = $file;
-                return true;
-            } catch(Exception $e) {
-                return false;
-            }
-        }
-        
-        return false;
-    }
-    
-    function getClassFile($class) {
-        if($this->knowsClass($class)) {
-            return self::$class_cache[$class];
-        }
-    }
-}
+require_once PHAKE_DIR_SRC . '/AutoLoader/Loader/Scripts.php';
 Phake_AutoLoader::addAutoloader(new Phake_AutoLoader_Loader_Scripts());
 
 
